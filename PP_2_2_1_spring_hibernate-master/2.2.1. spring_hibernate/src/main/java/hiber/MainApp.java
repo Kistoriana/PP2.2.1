d37ec2945ef35e1e3d0fad9d1cb20828a21c2757
Victoria Markova <kistoriana@gmail.com>
2023-06-21 20:23:41 +0300

    Initial commit

---
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,36 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("Ivan", "Ivanov", "ivan@mail.ru"), new Car("BMW", 5));
+      userService.add(new User("Marina", "Sidorova", "sidorovam@mail.ru"), new Car("KIA", 5));
+
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar().toString());
+      }
+
+      List<User> userList = userService.getUserByCar("KIA", 5);
+      System.out.println("Users Car = "+userList);
+      context.close();
+   }
+}
---
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,18 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface UserService {
+
+    @Transactional
+    void add(User user, Car car);
+
+    List<User> listUsers();
+
+    @Transactional(readOnly = true)
+    List<User> getUserByCar(String model, int series);
+}
